f0d319f58532a388d1b8fe382c95b10dcffb55ac
Cinthia <cinthiamashiel474@gmail.com>
2017-01-24 00:04:52 -0500

    agregado favicon y ejercicio funcional

--- js/main.js
+++ js/main.js
@@ -1,29 +1,55 @@
-function tareas(){
-    var datos = document.getElementById("message-text").value;
-    var lista = document.getElementById("lista");
-    var eliminar = document.getElementsByTagName("eliminar");
-    var checkbox = document.getElementById("checkbox");
+    var tareas = document.getElementById("tareas");
+    var input = document.getElementById("mensaje");
     var boton = document.getElementById("boton");
-    
-        
-// funciones
-    function agregarTarea(){
-        alert("agregado");
+
+function agregarTarea(){
+// para buscar el valor de un input textarea.    
+    var inputActual = input.value;
+// para no validar en caso de que este vacio.
+    if(inputActual === ""){
+       input.setAttribute("placeholder", "No se pueden agregar tareas vacias");
+        input.className="error";
+        return false;
     }
-    function tachar(){
-        alert("tachado")
+    function comprobarInput(){
+        input.className = "";
+        input.setAttribute("placeholder", "Agrega tu Tarea");
     }
-    function eliminarTarea(){
-        alert("eliminado");
-    }
+    input.addEventListener("click", comprobarInput);
+// para agregar un div individual al contenedor tareas.    
+    var lista = document.createElement("div");
+    lista.className="listan";
+    tareas.appendChild(lista);
+// para agregar un checkbox al div lista.    
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    lista.appendChild(checkbox);
+// para agregar el texto/mensaje en el div lista.   
+    var small = document.createElement("small");
+    lista.appendChild(small);
+    small.appendChild(document.createTextNode(inputActual));
+// para agregar un icono en el div lista.  
+    var icono = document.createElement("span");
+    icono.className = "glyphicon  glyphicon-trash , icon , pull-right";
+    lista.appendChild(icono);
+// para borrar el contexto una vez añadido.
+    inputActual=input.value="";
+// para eliminar el div lista con clickear el icono.
+var eliminar = document.getElementsByClassName("icon");
+for (var c = 0; c < eliminar.length; c++) {
+  eliminar[c].onclick = function(){
+    var lista = this.parentElement;
+    lista.style.display = "none";
+  }  
 }
-// agregar tarea
-    boton.addEventListener("clic", agregarTarea);
-// tachar input
-    checkbox.addEventListener("click",tachar);
-//  elimnimar tarea
-    eliminar.addEventListener("click", eliminarTarea);
- // borrando elementos de la lista   
-    for(var x=0; x<=lista.children.length-1; x++){
-      lista.children[i].addEventListener("click", eliminarTarea); 
-    }
+    checkbox.addEventListener("click", tachar);
+}
+// funcion para el tachado y destachado de las tareas.
+function tachar(evento){
+	var tachado = evento.target.parentNode;
+	if(evento.target.checked)
+		tachado.setAttribute("style", "text-decoration: overline underline line-through ");
+	else
+		tachado.setAttribute("style","text-decoration:none");
+}
+boton.addEventListener("click",  agregarTarea);
