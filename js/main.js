c38741b1b03593424ca2a0c909c18bf415fcb55a
Cinthia <cinthiamashiel474@gmail.com>
2017-02-06 16:05:40 -0500

    no validar campo si esta vacio

--- js/main.js
+++ js/main.js
@@ -3,17 +3,22 @@ var listaTareas =[];
 // funcion principal:
 function clickButton(){
     var inputMensaje = document.getElementById("mensaje").value;
-    listaTareas.push({nombre:inputMensaje,isDone:false});
-    drawTextList();
-    var inputMensaje = document.getElementById("mensaje").value="";
+    if(inputMensaje === ""){
+        alert("No se puede agregar la tarea si el campo esta vacio");
+        return false;
+    }else{
+       listaTareas.push({nombre:inputMensaje,isDone:false});
+        drawTextList();
+        var inputMensaje = document.getElementById("mensaje").value="";
+    }
 }
 // funcion para crear elementos (nuevo método):
 function drawTextList(){
    var lista = document.getElementById("lista");
    lista.innerHTML = "";
  
-   for(var c in listaTareas){
-      var html = "<li> <input type='checkbox' onclick='strikethrough("+c+")' "+(listaTareas[c].isDone?"checked":"")+">"+"<span "+(listaTareas[c].isDone?"style='text-decoration: overline underline line-through'":"style='text-decoration:none'")+" id='new'>"+listaTareas[c].nombre+"</span></input>"+"<span onclick='deleteTasks("+c+")' class='glyphicon  glyphicon-trash , icon , pull-right'>" + "</span>" +"</li>"
+   for(var i in listaTareas){
+      var html = "<li> <input type='checkbox' onclick='strikethrough("+i+")' "+(listaTareas[i].isDone?"checked":"")+">"+"<span "+(listaTareas[i].isDone?"style='text-decoration: overline underline line-through'":"style='text-decoration:none'")+" id='new'>"+listaTareas[i].nombre+"</span></input>" + "<span onclick='deleteTasks("+i+")' class='glyphicon  glyphicon-trash , icon , pull-right'>" + "</span>" +"</li>"
       lista.innerHTML += html;
     }
 }
